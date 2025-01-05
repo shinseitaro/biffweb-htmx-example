038411ab191819107c82429b4045b3f8bbe118a5
shinseitaro <shinsei.all@gmail.com>
2025-01-05 16:50:49 +0900

    new branch : updating other content solution 1

--- src/com/example/htmx/updating_other_content.clj
+++ src/com/example/htmx/updating_other_content.clj
@@ -13,40 +13,42 @@
   (let [contacts (fetch-data db)]
     (ui/page
      {}
-     [:h2 "Contacts"]
-     [:div {:class "h-1"}]
-     [:div {:class "overflow-x-auto"}
-      [:table {:class "table"}
-       [:thead
-        [:tr
-         [:th "First Name"]
-         [:th "Last Name"]
-         [:th "Email"]]]
-       [:tbody {:id "contacts-table"}
-        (for [contact contacts]
-          (let [{:contact/keys [first-name last-name email]} contact]
-            [:tr
-             [:td first-name]
-             [:td last-name]
-             [:td email]]))]]]
-     [:div {:class "h-3"}]
-     (biff/form
-      {}
-      [:div {:class "flex flex-col gap-1"}
-       [:h2  "Add Contact"]
-       [:input {:type        "text"
-                :name        "first-name"
-                :placeholder "First Name"}]
+    ;;  解決方法１：ターゲットを拡張
+     [:div {:id "table-and-form"}
+      [:h2 "Contacts"]
+      [:div {:class "h-1"}]
+      [:div {:class "overflow-x-auto"}
+       [:table {:class "table"}
+        [:thead
+         [:tr
+          [:th "First Name"]
+          [:th "Last Name"]
+          [:th "Email"]]]
+        [:tbody {:id "contacts-table"}
+         (for [contact contacts]
+           (let [{:contact/keys [first-name last-name email]} contact]
+             [:tr
+              [:td first-name]
+              [:td last-name]
+              [:td email]]))]]]
+      [:div {:class "h-3"}]
+      (biff/form
+       {}
+       [:div {:class "flex flex-col gap-1"}
+        [:h2  "Add Contact"]
+        [:input {:type        "text"
+                 :name        "first-name"
+                 :placeholder "First Name"}]
 
-       [:input {:type        "text"
-                :name        "last-name"
-                :placeholder "Last Name"}]
+        [:input {:type        "text"
+                 :name        "last-name"
+                 :placeholder "Last Name"}]
 
-       [:input {:type        "text"
-                :name        "email"
-                :placeholder "Email"}]
-       [:div
-        [:button {:class "btn"} "New Data"]]]))))
+        [:input {:type        "text"
+                 :name        "email"
+                 :placeholder "Email"}]
+        [:div
+         [:button {:class "btn"} "New Data"]]])])))
 
 
 (def module
