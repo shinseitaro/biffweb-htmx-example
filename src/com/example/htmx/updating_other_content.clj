84c82df452d37b699cdbabdd427971484bffac59
shinseitaro <shinsei.all@gmail.com>
2025-01-06 09:38:14 +0900

    add todo

--- src/com/example/htmx/updating_other_content.clj
+++ src/com/example/htmx/updating_other_content.clj
@@ -2,56 +2,96 @@
   (:require [com.biffweb :as biff]
             [com.example.ui :as ui]))
 
+
 (defn fetch-data [db]
   (->> (biff/q db
                '{:find  (pull e [*])
                  :where [[e :contact/email]]})
        (sort-by :contact/first-name)))
 
+;; (defn contact-list [db]
+;;   (let [contacts (fetch-data db)]
+;;     [:div {:class "overflow-x-auto"}
+;;      [:table {:class "table"}
+;;       [:thead
+;;        [:tr
+;;         [:th "First Name"]
+;;         [:th "Last Name"]
+;;         [:th "Email"]]]
+;;       [:tbody {:id "contacts-table"}
+;;        (for [contact contacts]
+;;          (let [{:contact/keys [first-name last-name email]} contact]
+;;            [:tr
+;;             [:td first-name]
+;;             [:td last-name]
+;;             [:td email]]))]]]))
+
+(defn contact-list [db]
+  [:div {:id "table-and-form"} ;; 全体をラップ、id をつける。
+   [:h2 "Contacts"]
+   [:div {:class "h-1"}]
+   (let [contacts (fetch-data db)]
+     [:div {:class "overflow-x-auto"}
+      [:table {:class "table"}
+       [:thead
+        [:tr
+         [:th "First Name"]
+         [:th "Last Name"]
+         [:th "Email"]]]
+       [:tbody {:id "contacts-table"}
+        (for [contact contacts]
+          (let [{:contact/keys [first-name last-name email]} contact]
+            [:tr
+             [:td first-name]
+             [:td last-name]
+             [:td email]]))]]])
+   [:div {:class "h-3"}]
+   (biff/form
+    {:hx-post   "/updating-other-content/contacts"
+     :hx-target "#table-and-form"} ;; コンタクトテーブルを target にする
+    [:div {:class "flex flex-col gap-1"}
+     [:h2  "Add Contact"]
+     [:input {:type        "text"
+              :name        "first-name"
+              :placeholder "First Name"}]
+
+     [:input {:type        "text"
+              :name        "last-name"
+              :placeholder "Last Name"}]
+
+     [:input {:type        "text"
+              :name        "email"
+              :placeholder "Email"}]
+     [:div
+      [:button {:class "btn"} "New Data"]]])])
+
+(defn submit-new-contact [ctx first-name last-name email]
+  (biff/submit-tx ctx
+                  [{:db/doc-type  :contact
+                    :db.op/upsert {:contact/first-name first-name
+                                   :contact/last-name  last-name
+                                   :contact/email      email
+                                   :contact/status     true}}])) ;; とりあえずtrue
+
+
+(defn new-contact [{:keys [params]
+                    :as   ctx}]
+  (let [{:keys [first-name last-name email]} params]
+    (submit-new-contact ctx first-name last-name email))
+  ;; TODO: 2025/01/06 ここの説明をZENNに書きつつSolution１を作成。マージコンテクストの使い方。
+  (biff/render (contact-list (:biff/db (biff/merge-context ctx)))))
+
 (defn app [{:keys [biff/db]
             :as   ctx}]
-  (let [contacts (fetch-data db)]
-    (ui/page
-     {}
-    ;;  解決方法１：ターゲットを拡張
-     [:div {:id "table-and-form"}
-      [:h2 "Contacts"]
-      [:div {:class "h-1"}]
-      [:div {:class "overflow-x-auto"}
-       [:table {:class "table"}
-        [:thead
-         [:tr
-          [:th "First Name"]
-          [:th "Last Name"]
-          [:th "Email"]]]
-        [:tbody {:id "contacts-table"}
-         (for [contact contacts]
-           (let [{:contact/keys [first-name last-name email]} contact]
-             [:tr
-              [:td first-name]
-              [:td last-name]
-              [:td email]]))]]]
-      [:div {:class "h-3"}]
-      (biff/form
-       {}
-       [:div {:class "flex flex-col gap-1"}
-        [:h2  "Add Contact"]
-        [:input {:type        "text"
-                 :name        "first-name"
-                 :placeholder "First Name"}]
+  (ui/page
+   {}
+   ;;  解決方法１：ターゲットを拡張
+   (contact-list db)))
 
-        [:input {:type        "text"
-                 :name        "last-name"
-                 :placeholder "Last Name"}]
-
-        [:input {:type        "text"
-                 :name        "email"
-                 :placeholder "Email"}]
-        [:div
-         [:button {:class "btn"} "New Data"]]])])))
 
 
 (def module
   {:routes ["/updating-other-content"
-            ["/" {:get app}]]})
+            ["/" {:get app}]
+            ["/contacts" {:post new-contact}]]})  
 
