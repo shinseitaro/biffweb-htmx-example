768c210f6b5e66e3a401f59b17d3320d662e8f56
shinseitaro <shinsei.all@gmail.com>
2025-01-11 12:43:27 +0900

    add solution 2

--- src/com/example/htmx/updating_other_content.clj
+++ src/com/example/htmx/updating_other_content.clj
@@ -1,12 +1,36 @@
 (ns com.example.htmx.updating-other-content
   (:require [com.biffweb :as biff]
-            [com.example.ui :as ui]))
+            [com.example.ui :as ui]
+            [rum.core :as rum]))
 
 (defn fetch-data [db]
   (->> (biff/q db
                '{:find  (pull e [*])
                  :where [[e :contact/email]]})
        (sort-by :contact/first-name)))
+
+(defn table-row [params]
+  (let [{:keys [first-name last-name email]} params]
+    [:tr
+     [:td first-name]
+     [:td last-name]
+     [:td email]]))
+
+(defn submit-new-contact [ctx first-name last-name email]
+  (biff/submit-tx ctx
+                  [{:db/doc-type  :contact
+                    :db.op/upsert {:contact/first-name first-name
+                                   :contact/last-name  last-name
+                                   :contact/email      email
+                                   :contact/status     true}}]))
+
+(defn new-contact [{:keys [params]
+                    :as   ctx}]
+  (let [{:keys [first-name last-name email]} params]
+    (submit-new-contact ctx first-name last-name email))
+  (biff/render [:tbody
+                {:hx-swap-oob "beforeend:#contacts-table"}
+                (table-row params)]))
 
 (defn app [{:keys [biff/db]
             :as   ctx}]
@@ -29,27 +53,30 @@
              [:td first-name]
              [:td last-name]
              [:td email]]))]]]
-     [:div {:class "h-3"}]
-     (biff/form
-      {}
-      [:div {:class "flex flex-col gap-1"}
-       [:h2  "Add Contact"]
-       [:input {:type        "text"
-                :name        "first-name"
-                :placeholder "First Name"}]
 
-       [:input {:type        "text"
-                :name        "last-name"
-                :placeholder "Last Name"}]
+     [:div {:class "h-3"}
+      (biff/form
+       {:hx-post "/updating-other-content/contacts"
+        :hx-swap "none"}
+       [:div {:class "flex flex-col gap-1"}
+        [:h2  "Add Contact"]
+        [:input {:type        "text"
+                 :name        "first-name"
+                 :placeholder "First Name"}]
 
-       [:input {:type        "text"
-                :name        "email"
-                :placeholder "Email"}]
-       [:div
-        [:button {:class "btn"} "New Data"]]]))))
+        [:input {:type        "text"
+                 :name        "last-name"
+                 :placeholder "Last Name"}]
+
+        [:input {:type        "text"
+                 :name        "email"
+                 :placeholder "Email"}]
+        [:div
+         [:button {:class "btn"} "New Data"]]])])))
 
 
 (def module
   {:routes ["/updating-other-content"
-            ["/" {:get app}]]})
+            ["/" {:get app}]
+            ["/contacts" {:post new-contact}]]})
 
